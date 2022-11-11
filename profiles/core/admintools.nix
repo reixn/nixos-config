1a1274cacda64db9394ef7291a34e6c4919acd3d
reixn <reixn@outlook.com>
2022-11-11 09:19:14 +0800

    [profiles] core/admintools: add tools Add: bat, dog, delta, duf, dust, exa, httpie

--- profiles/core/admintools.nix
+++ profiles/core/admintools.nix
@@ -2,11 +2,18 @@
 {
   # Selection of sysadmin tools that can come in handy
   environment.systemPackages = with pkgs; [
+    bat
     curl
     direnv
+    dogdns
+    delta
+    duf
+    du-dust
     dnsutils
+    exa
     fd
     htop
+    httpie
     bottom
     jq
     neovim
