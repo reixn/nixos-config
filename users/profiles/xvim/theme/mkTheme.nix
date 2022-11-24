7951ae159d057160dfd931dd14279a3056fcf7bf
reixn <reixn@outlook.com>
2022-11-24 11:34:45 +0800

    [user] profiles/xvim/theme: ignore vscode

--- users/profiles/xvim/theme/mkTheme.nix
+++ users/profiles/xvim/theme/mkTheme.nix
@@ -1,6 +1,13 @@
 { name, pkg }: { enable ? false }: name: { pkgs, lib, ...}: {
   xvim.${name}.base = {
     plugins = [ pkgs.vimPlugins.${pkg} ];
-    configs = lib.optional enable [ "colorscheme ${name}" ];
+    configs = lib.optional enable [ 
+      ''
+        if exists('g:vscode')
+        else
+          colorscheme ${name}
+        end
+      ''
+    ];
   };
 }
--- users/profiles/xvim/theme/moonfly.nix
+++ users/profiles/xvim/theme/moonfly.nix
@@ -1,9 +1,15 @@
 { enable ? false }: name: {pkgs, lib, ...}: {
   xvim.${name}.base = {
     plugins = [ pkgs.vimPlugins.vim-moonfly-colors ];
-    configs = builtins.filter (v: v != "") [ 
+    configs = builtins.filter (v: v != "") [
       "let g:moonflyTransparent = 1"
-      (lib.optionalString enable "colorscheme moonfly")
+      (lib.optionalString enable 
+        ''
+          if exists('g:vscode')
+          else
+            colorscheme moonfly
+          end
+        '')
     ];
   };
 }
