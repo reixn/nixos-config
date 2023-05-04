faf809696a18bd6f01d1fef2b0914374b1e767d5
Kyle Rogers <reixn@outlook.com>
2023-05-04 23:26:53 +0800

    [home] profiles/firefox: update search setting
    set default to Global Bing
    remove search bar

--- users/profiles/firefox/default.nix
+++ users/profiles/firefox/default.nix
@@ -7,12 +7,6 @@
           "browser.download.useDownloadDir" = false;
           "browser.download.start_downloads_in_tmp_dir" = true;
 
-          # add search in navbar
-          "browser.search.widget.inNavBar" = true;
-          "browser.uiCustomization.state" = ''
-            {"placements":{"widget-overflow-fixed-list":[],"nav-bar":["back-button","forward-button","stop-reload-button","customizableui-special-spring1","urlbar-container","search-container","customizableui-special-spring2","save-to-pocket-button","downloads-button","fxa-toolbar-menu-button"],"toolbar-menubar":["menubar-items"],"TabsToolbar":["tabbrowser-tabs","new-tab-button","alltabs-button"],"PersonalToolbar":["import-button","personal-bookmarks"]},"seen":["save-to-pocket-button","developer-button"],"dirtyAreaCache":["nav-bar","PersonalToolbar","toolbar-menubar","TabsToolbar"],"currentVersion":17,"newElementCount":2}
-          '';
-
           # delete cookies on close
           "network.cookie.lifetimePolicy" = 2;
           # don't remember history
@@ -61,7 +55,7 @@
             };
           };
           force = true;
-          default = "Bing";
+          default = "Bing Global";
         };
         isDefault = true;
       };
