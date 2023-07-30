c80171ef9a015ee77f95245a7e784143801719b8
Kyle Rogers <reixn@outlook.com>
2023-07-30 17:37:48 +0800

    [home] vscode/base: add editorconfig

--- users/profiles/vscode/base.nix
+++ users/profiles/vscode/base.nix
@@ -1,6 +1,7 @@
 { pkgs, ...}: {
   programs.vscode = {
     enable = true;
+    extensions = [ pkgs.vscode-extensions.editorconfig.editorconfig ];
     userSettings = {
       "editor.fontFamily" = "'Cascadia Code', 'Droid Sans Mono', 'monospace', monospace";
       "editor.fontLigatures" = true;
