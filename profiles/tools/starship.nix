3da1bd4166258869d7db279bd9557665a1f65822
Kyle Rogers <reixn@outlook.com>
2023-07-30 17:33:58 +0800

    [profile] starship: add shell,shlvl

--- profiles/tools/starship.nix
+++ profiles/tools/starship.nix
@@ -3,6 +3,14 @@
     enable = true;
     settings = {
       username.show_always = true;
+      shell = {
+        disabled = false;
+        style = "cyan bold";
+      };
+      shlvl = {
+        disabled = false;
+        threshold = 1;
+      };
     };
   };
 }
